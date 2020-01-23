786564d3e563370084ac9198c518167777037cb6
hamed pishdad <hamed.hpn332@gmail.com>
2020-01-23 16:55:07 +0330

    init

---
+++ image_viewer.gd
@@ -0,0 +1,21 @@
+extends VBoxContainer
+
+
+export(PackedScene) var image_holder = preload("res://object/ImageHolder.tscn")
+
+
+func add_images(path, images_list):
+	for child in get_children():
+		child.free()
+	
+	for image_name in images_list:
+		add_image(path + '/' + image_name)
+
+func add_image(path_file):
+	
+	var new = image_holder.instance()
+	add_child(new)
+	new.loading(path_file)
+	
+	
+	
---
+++ script/image_list.gd
@@ -0,0 +1,82 @@
+extends CanvasLayer
+
+#var file_dialog := FileDialog.new()
+onready var file_dialog := $FileDialog
+onready var vb = $VBoxContainer2/ScrollContainer/VBoxContainer
+
+
+#var image_list :Array= []
+
+func _init() -> void:
+#	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
+#	file_dialog.mode = FileDialog.MODE_OPEN_DIR
+#	file_dialog.resizable = true
+#	file_dialog.connect("dir_selected", self, '_on_FileDialog_dir_selected')
+#
+#	add_child(file_dialog)
+	pass
+
+func _process(delta):
+	var viewport = get_tree().root.get_viewport()
+	var m = viewport.get_mouse_position()
+	var z = m.x/viewport.size.x
+	
+	
+
+
+func _on_FileDialog_dir_selected(path: String) -> void:
+	
+	var regex = RegEx.new()
+	var image_list:= []
+	var dir := Directory.new()
+	
+#	print('path: ' + di)
+	if dir.open(path) == OK:
+		
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while (file_name != ""):
+			if not dir.current_is_dir():
+#				print("Found directory: " + file_name)
+#			else:
+				
+#				print(file_name)
+				regex.compile('[*.](png|jpg)$')
+				var result = regex.search(file_name)
+				
+				if result:
+					image_list.append(file_name)
+				
+#				
+#				print("Found file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	
+	vb.add_images(path, image_list)
+	
+	
+#	print('=====last')
+#	for i in image_list:
+#		print(i)
+	
+
+
+func _on_Button_pressed() -> void:
+	file_dialog.popup_centered()
+	
+	
+
+var zoom = 0.4
+
+func _on_zin_pressed() -> void:
+	zoom += .2
+	set_image_scale()
+
+
+func _on_zout_pressed() -> void:
+	zoom -= .2
+	set_image_scale()
+
+func set_image_scale():
+	vb.rect_scale = Vector2.ONE * zoom
