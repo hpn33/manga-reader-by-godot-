6cad81b74437f1284c69968cf7e655439c56c110
hamed pishdad <hamed.hpn332@gmail.com>
2020-01-26 14:50:17 +0330

    add title bar

---
+++ TitleBar.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var following := false
+var draging_start_position = Vector2()
+
+
+func _on_TitleBar_gui_input(event: InputEvent) -> void:
+	print(event)
+	if event is InputEventMouseButton:
+		if event.get_button_index() == 1:
+			following = !following
+			draging_start_position = get_local_mouse_position()
+	
+
+func _process(delta: float) -> void:
+	if following:
+		OS.window_position = OS.window_position + get_local_mouse_position() - draging_start_position
+		
+		
+		
+
+
+func _on_Button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_minimaiz_pressed() -> void:
+	OS.window_minimized = true
