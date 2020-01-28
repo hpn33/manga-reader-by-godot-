be1257a2d7babd6f76d43eec846b4ded1e7da696
hamed pishdad <hamed.hpn332@gmail.com>
2020-01-28 11:46:02 +0330

    clean title

--- scene/ui/Title.gd
+++ scene/ui/Title.gd
@@ -1,7 +1,6 @@
 extends Button
 
 onready var anim = $AnimationPlayer
-onready var tween = $Tween
 
 
 func _ready() -> void:
@@ -10,13 +9,7 @@ func _ready() -> void:
 
 func set_title(title: String):
 	text = title
-#	show()
 	anim.play("show")
-
-
-func show():
-	tween.interpolate_property(self, 'position:y', 20, 3, 1, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
-	tween.start()
 
 
 func _on_Title_pressed() -> void:
