2f67e64cc039c838e6875d0f2ed520b274fc80ea
hamed pishdad <hamed.hpn332@gmail.com>
2020-04-22 15:24:02 +0430

    improve NavBar

--- object/ImageView/NavBar/NavBar.gd
+++ object/ImageView/NavBar/NavBar.gd
@@ -1,8 +1,16 @@
 extends VBoxContainer
 
 
-onready var index_label = $"Current"
-onready var count_label = $"All"
+onready var index_label = $Current
+onready var count_label = $All
+
+onready var first_btn = $First
+onready var ten_prv_btn = $TenPrv
+onready var prv_btn = $Prv
+
+onready var end_btn = $End
+onready var ten_next_btn = $TenNext
+onready var next_btn = $Next
 
 
 var index := -1
@@ -29,6 +37,16 @@ func _on_First_pressed():
 	refresh()
 
 
+func _on_TenPrv_pressed():
+	if count == 0:
+		index = -1
+		return
+	
+	index = clamp(index - 10, 0, count-1)
+	
+	refresh()
+
+
 func _on_Prev_pressed():
 	
 	if count == 0:
@@ -51,6 +69,16 @@ func _on_Next_pressed():
 	refresh()
 
 
+func _on_TenNext_pressed():
+	if count == 0:
+		index = -1
+		return
+	
+	index = clamp(index + 10, 0, count-1)
+	
+	refresh()
+
+
 func _on_End_pressed():
 	
 	if count == 0:
@@ -62,12 +90,33 @@ func _on_End_pressed():
 	refresh()
 
 
-
 func refresh(can_go = true):
 	index_label.text = str(clamp(index, -1, count - 1) + 1)
 	
+	check_buttons()
+	
 	if can_go:
 		owner.notify(self)
+
+
+func check_buttons():
+	
+	disable_buttons(index == -1)
+	
+	if index == -1:
+		return
+	
+	first_btn.disabled = (index == 0)
+	prv_btn.disabled = (index == 0)
+	
+	ten_prv_btn.disabled = not (index-10 >= 0)
+	ten_next_btn.disabled = not (index+10 <= count-1)
+	
+	next_btn.disabled = (index == count-1)
+	end_btn.disabled = (index == count-1)
+	
+	
+	
 
 
 func set_current_index(_index):
@@ -80,6 +129,14 @@ func set_current_index(_index):
 	refresh(false)
 
 
+func disable_buttons(disable: bool) -> void:
+	for child in get_children():
+		if not child is Button:
+			continue
+		
+		child.disabled = disable
+		
+
 
 func notify(last):
 	
@@ -87,3 +144,4 @@ func notify(last):
 		return
 	
 	set_current_index(owner.get_focus_index())
+
