fab53ff2683eb6a38f45ca3a8324aa4cccb1ad93
hamed pishdad <hamed.hpn332@gmail.com>
2020-04-29 06:23:20 +0430

    init button for show file menu

---
+++ scene/FileMenu.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_FileMenu_pressed():
+	pass # Replace with function body.
--- scene/ui/UI.gd
+++ scene/ui/UI.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 onready var color_picker = $PopMenu/ColorPicker
 onready var info_panel = $PopMenu/InfoPanel
 onready var file_dialog = $PopMenu/FileDialog
+onready var file_menu = $PopMenu/FileMenu
 
 onready var title_bar = $TitleBar
 onready var info = $info
@@ -21,3 +22,7 @@ func open_info():
 
 func open_file_dialog():
 	file_dialog.popup_centered()
+
+
+func open_file_menu():
+	file_menu.popup_centered()
